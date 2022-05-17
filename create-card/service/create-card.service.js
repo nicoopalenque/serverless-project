d784766501dfed8ddb1915e7eb4218e43172b775
¨nicoopalenque¨ <nicoopalenque@gmail.com>
2022-05-17 19:21:05 -0300

    update credit card info

--- create-card/service/create-card.service.js
+++ create-card/service/create-card.service.js
@@ -3,24 +3,26 @@ const { creditCardNumber, expirationDate, securityCode } = require('../helper/ra
 const { setType } = require('../helper/card-type.helper');
 
 const createCardService = async (commandPayload) => {
-  const dbParams = {
-    dni: commandPayload.dni,
-    name: commandPayload.name,
-    lastName: commandPayload.lastName,
-    birth: commandPayload.birth,
-    creditCard: {
-      number: creditCardNumber(),
-      expiration: expirationDate(),
-      ccv: securityCode(),
-      type: setType(commandPayload),
+  console.log(commandPayload);
+  const params = {
+    TableName: process.env.CLIENTS_TABLE,
+    Key: { dni: commandPayload.dni },
+    ExpressionAttributeNames: {
+      "#C": "creditCard",
     },
-    status: 'ACTIVE',
+    ExpressionAttributeValues: {
+      ":c": {
+        number: creditCardNumber(),
+        expiration: expirationDate(),
+        ccv: securityCode(),
+        type: setType(commandPayload),
+      },
+    },
+    ReturnValues: "ALL_NEW",
+    UpdateExpression: 'SET #C = :c',
   }
 
-  return await dynamodb.putItem({
-    TableName: process.env.CLIENTS_TABLE,
-    Item: dbParams,
-  })
+  await dynamodb.updateItem(params);
 }
 
 module.exports = { createCardService };
