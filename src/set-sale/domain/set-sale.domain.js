84d42951df8d04b4d9ddd71d59852798e96f1292
¨nicoopalenque¨ <nicoopalenque@gmail.com>
2022-05-20 13:39:33 -0300

    refactor sale function

--- src/set-sale/domain/set-sale.domain.js
+++ src/set-sale/domain/set-sale.domain.js
@@ -4,11 +4,12 @@ const { SaleValidation } = require('../schema/input/set-sale.input');
 const { getClientService } = require('../service/get-client.service');
 const { validateClient } = require('../helper/validate-client.helper');
 const { setSaleService } = require('../service/set-sale.service');
+const { getClientCommon } = require('../../common/get-client.common');
 
 const setSaleDomain = async (commandPayload, commandMeta) => {
   new SaleValidation(commandPayload, commandMeta);
 
-  const client = await getClientService(commandPayload);
+  const client = await getClientCommon(commandPayload.dni);
   const isActive = validateClient(client);
 
   if(isActive) {
