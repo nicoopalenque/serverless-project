a7867e938c504a357565dadee6812c45d26149fd
¨nicoopalenque¨ <nicoopalenque@gmail.com>
2022-05-20 14:37:23 -0300

    function for set point

--- src/set-sale/domain/set-sale.domain.js
+++ src/set-sale/domain/set-sale.domain.js
@@ -1,8 +1,8 @@
 const { ErrorHandled } = require('ebased/util/error');
 
 const { SaleValidation } = require('../schema/input/set-sale.input');
-const { getClientService } = require('../service/get-client.service');
 const { validateClient } = require('../helper/validate-client.helper');
+const { applyPoints } = require('../helper/apply-points.helper');
 const { setSaleService } = require('../service/set-sale.service');
 const { getClientCommon } = require('../../common/get-client.common');
 
@@ -14,9 +14,16 @@ const setSaleDomain = async (commandPayload, commandMeta) => {
 
   if(isActive) {
     const response = await setSaleService(client, commandPayload);
+
+    const { totalPurchase, points } = await applyPoints(response, client);
+
     return {
       statusCode: 200,
-      body: response
+      body: {
+        response,
+        totalPurchase,
+        points
+      }
     }
   }else{
     throw new ErrorHandled("Client can't buy", {
